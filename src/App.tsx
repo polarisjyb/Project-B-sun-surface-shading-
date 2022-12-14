ade87b70aee2021d2ccd57f74ccf5c0f7e61381d
Violet Ultra <evia_a@naver.com>
2022-12-14 17:20:24 +0900

    브라우저 출력 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
   <div className="bg-white">
     <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
       <div>
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Hello Jumper!</h2>
         <p className="mt-4 text-gray-500">
           The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated
           steel divider separates active cards from new ones, or can be used to archive important task lists.
