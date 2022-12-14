c8cd1d3fce4f5f5c90d1f88f9bd6edbf8093c1dd
Violet Ultra <evia_a@naver.com>
2022-12-14 15:22:29 +0900

    webpack.prod.js 파일 주석 처리 및 학습 중

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,6 +3,13 @@ const common = require("./webpack.common");
 const path = require("path");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+/*
+  css 파일을 추출해주는 플러그인. HTML에 CSS를 알아서 link 해준다.
+  mini-css-extract-plugin 플러그인은 JavaScript 파일 안에서 호출되는 스타일 코드를 청크(Chunk)에서 파일로 추출하므로 개발 중에는 플러그인을 사용하지 않는 것이 좋다.
+  즉, 개발이 끝난 후 배포 할 때 사용하면 좋다.
+  개발 모드 (dev) 에서는 CSS를 여러 번 수정하고 DOM에 <style> 요소의 코드로 주입하는 것이 훨씬 빨리 작동하므로 "style-loader"를 사용한다.
+*/
+
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
