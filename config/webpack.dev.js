2107d671b811438ae8f45dc4f02e1ba079c70c3f
Violet Ultra <evia_a@naver.com>
2022-12-14 14:32:59 +0900

    webpack.dev.js 파일 설정에 대한 주석 처리 및 학습

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,8 +1,8 @@
 const { merge } = require("webpack-merge");
-const common = require("./webpack.config");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: "development", // 개발 모드
   devtool: "inline-source-map",
   devServer: {
     open: true,
@@ -12,14 +12,23 @@ module.exports = merge(common, {
     historyApiFallback: true,
     liveReload: true,
   },
+  /*
+    output - 번들링되고 만들어지는 파일에 대한 정보를 명시
+    output.filename - 번들링 된 파일의 이름을 명시
+    output.publicPath - 브라우저에서 참조될 때 출력 디렉터리의 공용 URL을 지정
+  */
   output: {
     filename: "[name].[contenthash].js",
     publicPath: "/",
   },
+  // 모듈이 생성 될 때 요청과 일치하는 rule의 배열.
   module: {
     rules: [
       {
-        test: /\.(sa|sc|c)ss$/i,
+        // loader를 적용시킬 파일들을 정규식으로 명시
+        test: /\.css$/i,
+        
+        //  use : 사용할 loader
         // webpack에서 postcss를 읽어들이는 postcss-loader를 추가
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
