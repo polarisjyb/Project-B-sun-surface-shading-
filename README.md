00926c2678ee651a67e98a1fbc87c3557df8ca6b
Violet Ultra <evia_a@naver.com>
2022-12-15 16:14:00 +0900

    node 서버로 tailwind 사용 (CDN 방식) , CDN 개념 정리

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Project-B
+# node-tailwind
 
-교통 정보 제공 서비스
+node 서버로 tailwind 사용 (fs 모듈 방식), (CDN 방식)
 
 
-tailwind css 프레임 워크 사용
+** CDN(Contents Delivery Network) 이란?
 
-package.json
+- 데이터 사용량이 많은 애플리케이션의 웹 페이지 로드 속도를 높이는 서버 네트워크
 
-- tailwindcss는 tailwind css의 코어 패키지
-- postcss는 tailwind css를 우리가 알고 있는 순수 css로 변환해주는 전처리기 패키지
-- autoprefixer는 브라우저별로 다르게 지원되는 벤더 프리픽스를 자동으로 넣어주는 패키지
-- postcss-loader는 webpack이 postcss를 읽어들일 수 있도록 해주는 로더
+- 지리적 제약 없이 전 세계 사용자에게 빠르고 안전하게 콘텐츠를 전송할 수 있는 콘텐츠 전송 기술
+
+- 사용자가 웹 사이트를 방문할 때 해당 웹 사이트 서버의 데이터는 사용자의 컴퓨터에 도달하기 위해 인터넷을 통해 이동해야 한다. 그런데 사용자가 해당 서버에서 멀리 떨어져 있는 경우 동영상 또는 웹 사이트 이미지와 같은 대용량 파일을 로드하는 데 시간이 오래 걸린다. 대신 웹 사이트 콘텐츠는 지리적으로 사용자와 가까운 CDN 서버에 저장되며 컴퓨터에 훨씬 빨리 도달한다.
--- server/app.js
+++ server/app.js
@@ -3,30 +3,46 @@ const fs = require('fs');
 const port = process.env.PORT || 8800;
 
 const app = http.createServer((req, res) => {
-
-  const index = () => {
-    res.writeHead(200, {'Content-Type':'text/html; charset= utf-8'});
-    fs.readFile('../public/index.html', 'utf8', (err, data) => {
-      if(err) throw err;
-      res.end(data, 'utf-8');
-      console.log('루트');
-    });
-  };
-
-  const pageError = () => {
-    res.writeHead(404, {'Content-Type':'text/plain; charset=utf-8'});
-    res.end('주소가 없습니다.');
-    console.log('Not found');
-  };
+  
+  const index = fs.readFileSync('../public/index.html', 'utf8');
+  const css = fs.readFileSync('../output/output.css', 'utf8');
+  
+  console.log(req.url);
 
   if(req.method === 'GET') {
     if(req.url === '/') {
-      index();
-    } else {
-      pageError();
-    };
+      res.writeHead(200, {'Content-Type':'text/html; charset= utf-8'});
+      res.end(index)
+    }
+    else if (req.url === '/output/output.css') {
+      res.writeHead(200, {'Content-Type':'text/css; charset= utf-8'});
+      res.end(css)
+    }
   };
+  // const index = () => {
+  //   fs.readFile('../public/index.html', 'utf8', (err, data) => {
+  //     if (err) throw err;
+  //     res.end(data, 'utf-8');
+  //     console.log('루트');
+  //   });
+  //   res.writeHead(200, {'Content-Type':'text/html; charset= utf-8'});
+  // };
+  
+  // const css = () => {
+  //   fs.readFile('../output/output.css', {encoding: 'utf8'}, (err, data) => {
+  //     if (err) throw err;
+  //     res.end(data);
+  //     console.log('outputCss');
+  //   });
+  //   res.writeHead(200, {'Content-Type':'text/css'});
+  // };
 
+  // const pageError = () => {
+  //   res.writeHead(404, {'Content-Type':'text/plain; charset=utf-8'});
+  //   res.end('주소가 없습니다.');
+  //   console.log('Not found');
+  // };
+  
 });
 
 app.listen(port, (err) => {
