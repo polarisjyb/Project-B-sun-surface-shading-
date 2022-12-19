6095518e7b2476b194eb252ff01ae438c06dfeae
JYB <evia_a@naver.com>
2022-12-19 17:53:27 +0900

    mapbox 이용을 위한 렌더링 마크업, Token 환경 설정은 구현 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,42 @@
+import react, { useState } from "react";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map from 'react-map-gl';
+
 const App = () => {
+
+  const REACT_APP_MAPBOX_TOKEN = 'Your token'
+  
+  const [ viewport, setViewport ] = useState({
+    latitude: 37.532600,
+    longitude: 127.024612,
+    zoom: 12
+  });
+
   return (
-    <></>
+    <div className="Mapbox">
+      <Map
+        {...viewport}
+        mapboxAccessToken={REACT_APP_MAPBOX_TOKEN}
+        onMove={e => setViewport(e.viewState)}
+        style={{width: "600px", height:"400px" }}
+        mapStyle="mapbox://styles/polarislsj/clbueti1r001414t1npzeqehk"
+        >
+      </Map>
+
+      {/* <Map
+        {...viewport}
+        mapboxAccessToken={REACT_APP_MAPBOX_TOKEN}
+
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14
+        }}
+        style={{width: "100vw", height:"100vh" }}
+        mapStyle="mapbox://styles/polarislsj/clbueti1r001414t1npzeqehk"
+      /> */}
+    </div>
   );
-}
+};
+
 export default App;
