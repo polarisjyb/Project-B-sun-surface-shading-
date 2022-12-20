fbd90f454f42c3fed5f5f945dec2c87383511544
JYB <evia_a@naver.com>
2022-12-20 15:01:31 +0900

    App.tsx 파일에서 불필요한 렌더링 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
 import react, { useState } from "react";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import Map from 'react-map-gl';
 
 const App = () => {
-  
-  const [ viewport, setViewport ] = useState({
-    latitude: 37.532600,
-    longitude: 127.024612,
-    zoom: 12
-  });
 
   return (
-    <div className="Mapbox">
-      <Map
-        {...viewport}
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        onMove={e => setViewport(e.viewState)}
-        style={{width: "800px", height:"800px" }}
-        mapStyle="mapbox://styles/polarislsj/clbueti1r001414t1npzeqehk"
-        >
-      </Map>
-
-      {/* <Map
-        {...viewport}
-        mapboxAccessToken={REACT_APP_MAPBOX_TOKEN}
-
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14
-        }}
-        style={{width: "100vw", height:"100vh" }}
-        mapStyle="mapbox://styles/polarislsj/clbueti1r001414t1npzeqehk"
-      /> */}
-    </div>
+    <div></div>
   );
 };
 
