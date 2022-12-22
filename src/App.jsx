a56d6e27e21f77de111e4cf3504be26f20157ba8
JYB <evia_a@naver.com>
2022-12-22 14:38:16 +0900

    canvas 태그를 공통 부모 태그로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import react from "react";
-// import { Canvas } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import Sphere from "./components/Sphere";
+import { SpaceDust } from "./components/SpaceDust";
+
 
 // import { Canvas } from "@react-three/fiber";
 // import Sphere from "./components/Sphere";
 
 const App = () => {
   return (
-
-    <Sphere />
-  
-    );
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas camera={{
+        position: [50, 0, 50],
+        fov: 90,
+        near: 0.1,
+        far: 3000,}}>
+        <SpaceDust count={200} />
+        <Sphere />
+        <axesHelper args={[100, 100, 100]} />
+      </Canvas>
+    </div>
+  );
 };
 
 export default App;
